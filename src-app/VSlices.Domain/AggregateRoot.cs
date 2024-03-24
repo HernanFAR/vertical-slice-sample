40227221ff7a400a92a0f7dfe2124ae819775d00
Hernan <h.f.alvarez.rubio>
2024-03-24 19:10:35 -0300

    feat: deleted conflictive code inside of AggregateRoot.cs

--- src-app/VSlices.Domain/AggregateRoot.cs
+++ src-app/VSlices.Domain/AggregateRoot.cs
@@ -6,34 +6,16 @@ namespace VSlices.Domain;
 public abstract class AggregateRoot<TKey> : Entity<TKey>, IAggregateRoot<TKey>
     where TKey : struct, IEquatable<TKey>
 {
-    /// <inheritdoc />
-    public TKey Id { get; private set; }
-
     /// <summary>
     /// Empty constructor to use in serialization scenarios
     /// </summary>
     /// <remarks>Do not use this constructor in your code, if is not for serialization</remarks>
-    protected AggregateRoot()
-    {
-        Id = default!;
-    }
+    protected AggregateRoot() {}
 
     /// <summary>
     /// Creates a new entity with the specified key
     /// </summary>
     /// <param name="id">The key of the entity</param>
-    protected AggregateRoot(TKey id)
-    {
-        Id = id;
-    }
-
-    /// <inheritdoc/>
-    public override string ToString() => this.EntityToString();
-
-    /// <inheritdoc/>
-    public virtual bool Equals(IEntity<TKey>? other) => this.EntityEquals(other);
-
-    /// <inheritdoc/>
-    public virtual bool Equals(IEntity? other) => this.EntityEquals(other);
+    protected AggregateRoot(TKey id) : base(id) { }
 
 }
