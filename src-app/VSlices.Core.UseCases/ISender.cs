457c540a3e7cf16fddee9ca4c5957850210e328b
Hernan <h.f.alvarez.rubio>
2024-03-31 17:57:14 -0300

    feat: added unit tests of VSlices.Core.UseCases.Reflection

--- src-app/VSlices.Core.UseCases/ISender.cs
+++ src-app/VSlices.Core.UseCases/ISender.cs
@@ -10,10 +10,10 @@ public interface ISender
     /// <summary>
     /// Asynchronously sends a request to a handler
     /// </summary>
-    /// <typeparam name="TResponse">Expected response type</typeparam>
+    /// <typeparam name="TResult">Expected response type</typeparam>
     /// <param name="request">Request to be handled</param>
     /// <param name="cancellationToken">Cancellation Token</param>
     /// <returns>A <see cref="ValueTask{T}"/> that represents an asynchronous operations which returns a <see cref="Result{T}"/> of <see cref="Success"/> that represents the result of the operation </returns>
-    ValueTask<Result<TResponse>> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken);
+    ValueTask<Result<TResult>> SendAsync<TResult>(IRequest<TResult> request, CancellationToken cancellationToken);
 
 }
