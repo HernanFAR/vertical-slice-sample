96a6da5b2175dccc70bc418001f23e206ce3435e
Hernan <h.f.alvarez.rubio>
2024-03-31 15:27:35 -0300

    feat: added unit tests of VSlices.Core.Events.HostedEventListener

---
+++ VSlices.Core.Events.HostedEventListener.UnitTests/Extensions/HostedEventListenerExtensions.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VSlices.Core.Events;
+using VSlices.Core.Events.Configurations;
+using VSlices.Domain.Interfaces;
+
+namespace VSlices.Core.Abstracts.UnitTests.Extensions;
+
+public class HostedEventListenerExtensions
+{
+    [Fact]
+    public void AddDefaultHostedEventListener_ShouldAddEventListener_DetailWithoutConfig()
+    {
+        var services = new ServiceCollection();
+
+        services.AddDefaultHostedEventListener();
+
+        services.Where(e => e.ServiceType == typeof(IEventListenerCore))
+            .Where(e => e.ImplementationType == typeof(EventListenerCore))
+            .Any(e => e.Lifetime == ServiceLifetime.Singleton)
+            .Should().BeTrue();
+
+        services.Where(e => e.ServiceType == typeof(IHostedService))
+            .Where(e => e.ImplementationType == typeof(HostedEventListener))
+            .Any(e => e.Lifetime == ServiceLifetime.Singleton)
+            .Should().BeTrue();
+
+        var descriptor = services
+            .Where(e => e.ServiceType == typeof(EventListenerConfiguration))
+            .Single(e => e.Lifetime == ServiceLifetime.Singleton);
+
+        var opts = (EventListenerConfiguration)descriptor.ImplementationInstance!;
+
+        opts.ActionInException.Should().Be(MoveActions.MoveLast);
+        opts.MaxRetries.Should().Be(3);
+
+
+    }
+
+    [Fact]
+    public void AddDefaultHostedEventListener_ShouldAddEventListener_DetailWithConfig()
+    {
+        const MoveActions moveActions = MoveActions.ImmediateRetry;
+        var services = new ServiceCollection();
+
+        services.AddDefaultHostedEventListener(config =>
+        {
+            config.ActionInException = moveActions;
+        });
+
+        services.Where(e => e.ServiceType == typeof(IEventListenerCore))
+            .Where(e => e.ImplementationType == typeof(EventListenerCore))
+            .Any(e => e.Lifetime == ServiceLifetime.Singleton)
+            .Should().BeTrue();
+
+        services.Where(e => e.ServiceType == typeof(IHostedService))
+            .Where(e => e.ImplementationType == typeof(HostedEventListener))
+            .Any(e => e.Lifetime == ServiceLifetime.Singleton)
+            .Should().BeTrue();
+
+        var descriptor = services
+            .Where(e => e.ServiceType == typeof(EventListenerConfiguration))
+            .Single(e => e.Lifetime == ServiceLifetime.Singleton);
+
+        var opts = (EventListenerConfiguration)descriptor.ImplementationInstance!;
+
+        opts.ActionInException.Should().Be(moveActions);
+        opts.MaxRetries.Should().Be(3);
+
+
+    }
+
+}
