d7ad9a93fd16d921880b7fecba949d16a3f3df57
Hernan <h.f.alvarez.rubio>
2024-04-03 19:19:54 -0300

    feat: added unit tests of VSlices.Core.CrossCutting.Pipeline

---
+++ src-app/VSlices.CrossCutting.Pipeline/Extensions/FeatureBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VSlices.Base;
+using VSlices.CrossCutting.Pipeline;
+
+namespace VSlices.Core.Builder;
+
+/// <summary>
+/// <see cref="FeatureBuilder" /> extensions to simplify <see cref="IFeature{TResult}" />'s 
+/// <see cref="IPipelineBehavior{TFeature, TResult}" /> definitions
+/// </summary>
+public static class FeatureBuilderExtensions
+{
+    /// <summary>
+    /// Adds <typeparamref name="T"/> as <see cref="IPipelineBehavior{TRequest,TResult}"/> to the service collection.
+    /// </summary>
+    /// <typeparam name="T">The endpoint definition to be added</typeparam>
+    /// <param name="featureBuilder">Service collection</param>
+    /// <returns>Service collection</returns>
+    public static FeatureBuilder AddPipeline<T>(this FeatureBuilder featureBuilder)
+        => featureBuilder.AddPipeline(typeof(T));
+
+    /// <summary>
+    /// Adds an the specified <see cref="Type"/> as <see cref="IHandler{TRequest,TResult}"/> to the service collection.
+    /// </summary>
+    /// <param name="featureBuilder">Service collection</param>
+    /// <param name="handlerType">The endpoint definition to be added</param>
+    /// <exception cref="InvalidOperationException"></exception>
+    /// <returns>Service collection</returns>
+    public static FeatureBuilder AddPipeline(this FeatureBuilder featureBuilder,
+        Type handlerType)
+    {
+        var handlerInterface = handlerType.GetInterfaces()
+            .Where(o => o.IsGenericType)
+            .SingleOrDefault(o => o.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>));
+
+        if (handlerInterface is null)
+        {
+            throw new InvalidOperationException(
+                $"The type {handlerType.FullName} does not implement {typeof(IPipelineBehavior<,>).FullName}");
+        }
+
+        featureBuilder.Services.AddTransient(handlerInterface, handlerType);
+
+        return featureBuilder;
+
+    }
+}
---
+++ tests-app/VSlices.CrossCutting.Pipeline.UnitTests/Extensions/PipelineBehaviorExtensions.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using System.Diagnostics;
+using VSlices.Base;
+using VSlices.Base.Responses;
+using VSlices.Core.Builder;
+using VSlices.CrossCutting.Pipeline;
+using static VSlices.CrossCutting.UnitTests.Extensions.FeatureBuilderExtensions;
+
+namespace VSlices.CrossCutting.UnitTests.Extensions;
+
+public class FeatureBuilderExtensions
+{
+    public record RequestResult;
+    public record Request : IFeature<RequestResult>;
+    public class TestPipeline<TRequest, TResult> : IPipelineBehavior<TRequest, TResult>
+        where TRequest : IFeature<TResult>
+    {
+        public ValueTask<Result<TResult>> HandleAsync(TRequest request, 
+            RequestHandlerDelegate<TResult> next, CancellationToken cancellationToken)
+        {
+            throw new UnreachableException();
+        }
+    }
+
+    [Fact]
+    public void AddPipeline_ShouldRegisterInServiceContainer()
+    {
+        FeatureBuilder builder = new(new ServiceCollection());
+
+        builder.AddPipeline<TestPipeline<Request, RequestResult>>();
+
+        builder.Services
+            .Where(e => e.ServiceType == typeof(IPipelineBehavior<Request, RequestResult>))
+            .Any(e => e.ImplementationType == typeof(TestPipeline<Request, RequestResult>))
+            .Should().BeTrue();
+
+    }
+}
