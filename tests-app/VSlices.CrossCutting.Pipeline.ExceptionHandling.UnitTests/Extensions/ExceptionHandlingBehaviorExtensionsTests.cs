4e9705bf81541d3bf0fe1a367416a1b9922670f3
Hernan <h.f.alvarez.rubio>
2024-04-04 02:06:41 -0300

    feat: added more tests

--- tests-app/VSlices.CrossCutting.Pipeline.ExceptionHandling.UnitTests/Extensions/ExceptionHandlingBehaviorExtensionsTests.cs
+++ tests-app/VSlices.CrossCutting.Pipeline.ExceptionHandling.UnitTests/Extensions/ExceptionHandlingBehaviorExtensionsTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics;
 using VSlices.Base;
+using VSlices.Base.Responses;
 using VSlices.Core.Builder;
 
 namespace VSlices.CrossCutting.Pipeline.ExceptionHandling.UnitTests.Extensions;
@@ -10,12 +11,20 @@ public class ExceptionHandlingBehaviorExtensionsTests
 {
     public record RequestResult;
     public record Request : IFeature<RequestResult>;
-    public class TestPipeline<TRequest, TResult> : AbstractExceptionHandlingBehavior<TRequest, TResult>
+    public class TestPipeline1<TRequest, TResult> : AbstractExceptionHandlingBehavior<TRequest, TResult>
         where TRequest : IFeature<TResult>
     {
         protected internal override ValueTask ProcessExceptionAsync(Exception ex, TRequest request)
         {
             throw new UnreachableException();
+        }
+    }
+    public class TestPipeline2<TRequest, TResult> : IPipelineBehavior<TRequest, TResult>
+        where TRequest : IFeature<TResult>
+    {
+        public ValueTask<Result<TResult>> HandleAsync(TRequest request, RequestHandlerDelegate<TResult> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -24,12 +33,38 @@ public class ExceptionHandlingBehaviorExtensionsTests
     {
         FeatureBuilder builder = new(new ServiceCollection());
 
-        builder.AddExceptionHandlingPipeline<TestPipeline<Request, RequestResult>>();
+        builder.AddExceptionHandlingPipeline<TestPipeline1<Request, RequestResult>>();
 
         builder.Services
             .Where(e => e.ServiceType == typeof(IPipelineBehavior<Request, RequestResult>))
-            .Any(e => e.ImplementationType == typeof(TestPipeline<Request, RequestResult>))
+            .Any(e => e.ImplementationType == typeof(TestPipeline1<Request, RequestResult>))
             .Should().BeTrue();
+
+    }
+
+    [Fact]
+    public void AddExceptionHandlingPipeline_ShouldThrowInvalidOperation_DetailDoesNotImplementPipelineBehavior()
+    {
+        var expMessage = $"The type {typeof(object).FullName} does not implement {typeof(IPipelineBehavior<,>).FullName}";
+        
+        FeatureBuilder builder = new(new ServiceCollection());
+
+        var act = builder.AddExceptionHandlingPipeline<object>;
+
+        act.Should().Throw<InvalidOperationException>().WithMessage(expMessage);
+
+    }
+
+    [Fact]
+    public void AddExceptionHandlingPipeline_ShouldThrowInvalidOperation_DetailDoesNotImplementExceptionHandlingBehavior()
+    {
+        var expMessage = $"Type {typeof(TestPipeline2<Request, RequestResult>).FullName} must inherit from {typeof(AbstractExceptionHandlingBehavior<,>).FullName}";
+
+        FeatureBuilder builder = new(new ServiceCollection());
+
+        var act = builder.AddExceptionHandlingPipeline<TestPipeline2<Request, RequestResult>>;
+
+        act.Should().Throw<InvalidOperationException>().WithMessage(expMessage);
 
     }
 }
