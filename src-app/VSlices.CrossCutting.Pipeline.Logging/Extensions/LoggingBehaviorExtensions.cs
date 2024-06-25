e5cf428458074f119d1a3db7342758480d5b357d
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-06-24 23:57:00 -0400

    feat: added source code of LoggingBehavior

---
+++ src-app/VSlices.CrossCutting.Pipeline.Logging/Extensions/LoggingBehaviorExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using VSlices.Base;
+using VSlices.CrossCutting.Pipeline;
+using VSlices.CrossCutting.Pipeline.Logging;
+using VSlices.CrossCutting.Pipeline.Logging.MessageTemplates;
+
+// ReSharper disable once CheckNamespace
+namespace VSlices.Core.Builder;
+
+/// <summary>
+/// <see cref="FeatureBuilder"/> extensions for <see cref="LoggingBehavior{TRequest,TResult}"/>
+/// </summary>
+public static class LoggingBehaviorExtensions
+{
+    /// <summary>
+    /// Adds an open generic pipeline behavior to the service collection
+    /// </summary>
+    /// <param name="featureBuilder">Service Collection</param>
+    /// <returns>Service Collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static LoggingBehaviorBuilder AddLoggingBehaviorFor<T>(this FeatureBuilder featureBuilder)
+        where T : IFeature
+        => featureBuilder.AddLoggingBehaviorFor(typeof(T));
+    
+    /// <summary>
+    /// Adds an open generic pipeline behavior to the service collection
+    /// </summary>
+    /// <param name="featureBuilder">Service Collection</param>
+    /// <param name="feature">Behavior</param>
+    /// <returns>Service Collection</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static LoggingBehaviorBuilder AddLoggingBehaviorFor(this FeatureBuilder featureBuilder, Type feature)
+    {
+        Type featureInterface = feature
+            .GetInterfaces()
+            .Where(o => o.IsGenericType)
+            .SingleOrDefault(o => o.GetGenericTypeDefinition() == typeof(IFeature<>))
+            ?? throw new InvalidOperationException(
+                $"The type {feature.FullName} does not implement {typeof(IFeature<>).FullName}");
+
+        Type featureResultType = featureInterface.GetGenericArguments()[0];
+
+        Type loggingBehaviorType = typeof(LoggingBehavior<,>).MakeGenericType(feature, featureResultType);
+        Type pipelineBehaviorType = typeof(IPipelineBehavior<,>).MakeGenericType(feature, featureResultType);
+
+        featureBuilder.Services
+            .AddTransient(pipelineBehaviorType, loggingBehaviorType)
+            .TryAddSingleton(TimeProvider.System);
+
+        return new LoggingBehaviorBuilder(featureBuilder);
+
+    }
+}
+
+/// <summary>
+/// Builder for <see cref="LoggingBehavior{TRequest,TResult}"/>
+/// </summary>
+/// <param name="builder"></param>
+public sealed class LoggingBehaviorBuilder(FeatureBuilder builder)
+{
+    readonly FeatureBuilder _builder = builder;
+
+    /// <summary>
+    /// Add a custom <see cref="ILoggingMessageTemplate"/>
+    /// </summary>
+    public FeatureBuilder UsingTemplate<TMessageTemplate>()
+        where TMessageTemplate : class, ILoggingMessageTemplate
+    {
+        _builder.Services.AddSingleton<ILoggingMessageTemplate, TMessageTemplate>();
+
+        return _builder;
+    }
+
+    /// <summary>
+    /// Add a english <see cref="ILoggingMessageTemplate"/>
+    /// </summary>
+    public FeatureBuilder UsingEnglishTemplate()
+    {
+        _builder.Services.AddSingleton<ILoggingMessageTemplate, EnglishLoggingMessageTemplate>();
+
+        return _builder;
+    }
+
+    /// <summary>
+    /// Add a spanish <see cref="ILoggingMessageTemplate"/>
+    /// </summary>
+    public FeatureBuilder UsingSpanishTemplate()
+    {
+        _builder.Services.AddSingleton<ILoggingMessageTemplate, SpanishLoggingMessageTemplate>();
+
+        return _builder;
+    }
+}
---
+++ src-app/VSlices.CrossCutting.Pipeline.Logging/LoggingBehavior.cs
@@ -0,0 +1,65 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+using LanguageExt.SysX.Live;
+using Microsoft.Extensions.Logging;
+using VSlices.Base;
+using VSlices.Core;
+using VSlices.CrossCutting.Pipeline.Logging.MessageTemplates;
+
+namespace VSlices.CrossCutting.Pipeline.Logging;
+
+/// <summary>
+/// Logging behavior for <see cref="IHandler{TRequest, TResult}"/>
+/// </summary>
+/// <typeparam name="TRequest">Request to handle</typeparam>
+/// <typeparam name="TResult">Expected result</typeparam>
+/// <param name="messageTemplate">Message templates</param>
+/// <param name="logger">Logger</param>
+/// <param name="timeProvider">Time provider</param>
+public sealed class LoggingBehavior<TRequest, TResult>(
+    ILoggingMessageTemplate messageTemplate,
+    ILogger<TRequest> logger,
+    TimeProvider timeProvider)
+    : AbstractPipelineBehavior<TRequest, TResult>
+    where TRequest : IFeature<TResult>
+{
+    readonly ILoggingMessageTemplate _messageTemplate = messageTemplate;
+    readonly ILogger<TRequest> _logger = logger;
+    readonly TimeProvider _timeProvider = timeProvider;
+
+    /// <inheritdoc />
+    protected override Aff<Runtime, Unit> BeforeHandle(TRequest request) =>
+        from _ in Eff(() =>
+        {
+            _logger.LogInformation(_messageTemplate.Start, 
+                _timeProvider.GetUtcNow(), typeof(TRequest).FullName, request);
+
+            return unit;
+        })
+        select unit;
+
+    /// <inheritdoc />
+    protected override Aff<Runtime, TResult> AfterSuccessHandling(TRequest request, TResult result) =>
+        from _ in Eff(() =>
+        {
+            _logger.LogInformation(_messageTemplate.SuccessEnd,
+                _timeProvider.GetUtcNow(), typeof(TRequest).FullName, request, result);
+
+            return unit;
+        })
+        from result_ in SuccessEff(result)
+        select result_;
+
+    /// <inheritdoc />
+    protected override Aff<Runtime, TResult> AfterFailureHandling(TRequest request, Error result) =>
+        from _ in Eff(() =>
+        {
+            _logger.LogError(_messageTemplate.FailureEnd, 
+                _timeProvider.GetUtcNow(), typeof(TRequest).FullName, request, result);
+
+            return unit;
+        })
+        from result_ in EffMaybe<TResult>(() => result)
+        select result_;  
+}
