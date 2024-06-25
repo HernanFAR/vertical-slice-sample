addfcce46a06488057fd410a8f315d2371368a2a
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-06-24 23:57:00 -0400

    feat: added source code of LoggingBehavior

---
+++ src-app/VSlices.CrossCutting.Pipeline.Logging/MessageTemplates/ILoggingMessageTemplate.cs
@@ -0,0 +1,55 @@
+﻿using LanguageExt.Common;
+
+namespace VSlices.CrossCutting.Pipeline.Logging.MessageTemplates;
+
+/// <summary>
+/// Interface for logging message templates
+/// </summary>
+public interface ILoggingMessageTemplate
+{
+    /// <summary>
+    /// Invoked when the pipeline starts
+    /// </summary>
+    string Start { get; }
+
+    /// <summary>
+    /// Invoked when the handler executes with failure
+    /// </summary>
+    string FailureEnd { get; }
+
+    /// <summary>
+    /// Invoked when the handler executes correctly
+    /// </summary>
+    string SuccessEnd { get; }
+}
+
+/// <summary>
+/// English logging message template
+/// </summary>
+internal sealed class EnglishLoggingMessageTemplate : ILoggingMessageTemplate
+{
+    /// <inheritdoc />
+    public string Start => "{0} - Starting handling of {1} | Input: {2}";
+
+    /// <inheritdoc />
+    public string FailureEnd => "{0} - Finished handling of {1}, result: Failure | Input: {2} | Output: {3}";
+
+    /// <inheritdoc />
+    public string SuccessEnd => "{0} - Finished handling of {1}, result: Success | Input: {2} | Output: {3}";
+}
+
+
+/// <summary>
+/// Spanish logging message template
+/// </summary>
+internal sealed class SpanishLoggingMessageTemplate : ILoggingMessageTemplate
+{
+    /// <inheritdoc />
+    public string Start => "{0} - Ejecución de {1} iniciada | Entrada: {2}";
+
+    /// <inheritdoc />
+    public string FailureEnd => "{0} - Ejecución de {1} terminada, resultado: Fallo | Entrada: {2} | Salida: {3}";
+
+    /// <inheritdoc />
+    public string SuccessEnd => "{0} - Ejecución de {1} terminada, result: Exito | Entrada: {2} | Salida: {3}";
+}
