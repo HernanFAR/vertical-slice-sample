ec0499c3a875b25f13903e25c39ec915e7df73fd
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-07-05 08:19:50 -0400

    feat: added testing of new EventFiltering unit tests

---
+++ tests-app/VSlices.CrossCutting.Pipeline.EventFiltering.UnitTests/EventFilteringBehaviorTests.cs
@@ -0,0 +1,122 @@
+using FluentAssertions;
+using LanguageExt;
+using LanguageExt.Common;
+using LanguageExt.SysX.Live;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System.Globalization;
+using VSlices.Base.Failures;
+using VSlices.CrossCutting.Pipeline.EventFiltering.MessageTemplates;
+using VSlices.Domain;
+using static LanguageExt.Prelude;
+
+namespace VSlices.CrossCutting.Pipeline.EventFiltering.UnitTests;
+
+public class EventFilteringBehaviorTests
+{
+    public sealed record Request : Event;
+
+    public abstract class Logger : ILogger<Request>
+    {
+        void ILogger.Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception? exception,
+            Func<TState, Exception?, string> formatter)
+        {
+            string message = formatter(state, exception);
+            Log(logLevel, message);
+        }
+
+        public abstract void Log(LogLevel logLevel, string message);
+
+        public virtual bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public abstract IDisposable BeginScope<TState>(TState state);
+    }
+
+    readonly Logger _logger = Substitute.For<Logger>();
+    readonly IEventFilter<Request> _eventFilter = Substitute.For<IEventFilter<Request>>();
+    readonly TimeProvider _timeProvider = Substitute.For<TimeProvider>();
+
+    EventFilteringBehavior<Request> GetSut(IEventFilteringMessageTemplate template) 
+        => new(_eventFilter, template, _logger, _timeProvider);
+
+    public static IEnumerable<object[]> GetTemplates()
+    {
+        return
+        [
+            [EnglishEventFilteringMessageTemplate.Instance],
+            [SpanishEventFilteringMessageTemplate.Instance]
+        ];
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTemplates))]
+    public async Task Define_Success_ShouldLogInputAndSuccessOutput(IEventFilteringMessageTemplate template)
+    {
+        // Arrange
+        EventFilteringBehavior<Request> sut = GetSut(template);
+
+        DateTimeOffset expFirstTime = DateTimeOffset.Now.UtcDateTime;
+        Request request = new();
+
+        string expStartMessage = string.Format(template.ContinueExecution,
+            expFirstTime.ToString("MM/dd/yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture), typeof(Request).FullName, request);
+
+        _eventFilter.Define(request)
+            .Returns(trueAff);
+
+        _timeProvider.GetUtcNow()
+            .Returns(expFirstTime);
+
+        // Act
+        Fin<Unit> result = await sut
+            .Define(request, SuccessAff(unit))
+            .Run(Runtime.New());
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+
+        _logger.Received(1).Log(
+            LogLevel.Information,
+            expStartMessage);
+
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTemplates))]
+    public async Task Define_Success_ShouldLogInputAndFailureOutput(IEventFilteringMessageTemplate template)
+    {
+        // Arrange
+        EventFilteringBehavior<Request> sut = GetSut(template);
+
+        DateTimeOffset expFirstTime = DateTimeOffset.Now.UtcDateTime;
+        Request request = new();
+
+        string expStartMessage = string.Format(template.SkipExecution,
+            expFirstTime.ToString("MM/dd/yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture), typeof(Request).FullName, request);
+
+        _eventFilter.Define(request)
+            .Returns(falseAff);
+
+        _timeProvider.GetUtcNow()
+            .Returns(expFirstTime);
+
+        // Act
+        Fin<Unit> result = await sut
+            .Define(request, SuccessAff(unit))
+            .Run(Runtime.New());
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+
+        _logger.Received(1).Log(
+            LogLevel.Warning,
+            expStartMessage);
+    }
+}
