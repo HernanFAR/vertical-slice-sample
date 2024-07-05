5c4784746290786ea220a7374721142850ba81a7
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-07-05 05:28:25 -0400

    feat: added BackgroundTaskListener in cross-cutting

---
+++ src-app/VSlices.CrossCutting.BackgroundTaskListener.Hosting/Extensions/HostingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using VSlices.CrossCutting.BackgroundTaskListener;
+using VSlices.CrossCutting.BackgroundTaskListener.Hosting;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection"/> to register <see cref="HostedTaskListener"/>
+/// </summary>
+public static class HostingExtensions
+{
+    /// <summary>
+    /// Adds a <see cref="IBackgroundTaskListener"/> of type <typeparamref name="T"/>
+    /// </summary>
+    public static IServiceCollection AddHostedTaskListener(this IServiceCollection services)
+        => services.AddSingleton<IBackgroundTaskListener, HostedTaskListener>();
+}
