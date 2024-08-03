aef87366ac03086f6cfc81d874e92b31487bdeca
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-08-02 22:13:39 -0400

    Refactorización y nuevas pruebas con contenedor SQL Server
    Se han realizado las siguientes modificaciones:
    
    * En `EfCoreRepositoryTests.cs`:
      * Añadidas y eliminadas declaraciones `using`.
      * Modificada la clase para implementar `IClassFixture<Fixture>`.
      * Añadidos varios métodos de prueba.
      * Eliminadas clases anidadas `EntityId`, `Entity`, `TEntity`, `Context` y `EntityRepository`.
    
    * En `VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests.csproj`:
      * Añadida referencia a `FluentAssertions`.
      * Reemplazada referencia a `Microsoft.EntityFrameworkCore.InMemory` por `Microsoft.EntityFrameworkCore.Design` y `Microsoft.EntityFrameworkCore.SqlServer`.
      * Añadida referencia a `Testcontainers.MsSql`.
    
    * Nuevos archivos:
      * `Fixture.cs`: Clase `Fixture` que implementa `IAsyncLifetime` para gestionar contenedor SQL Server.
      * `Context.cs`: Clase `Context` que hereda de `DbContext` y configura base de datos SQL Server.
      * `EntityRepository.cs`: Clase `EntityRepository` que hereda de `EfCoreRepository`.
      * `TestUnitOfWork.cs`: Clase `TestUnitOfWork` que hereda de `EfCoreUnitOfWork`.
      * `ContextModelSnapshot.cs`: Instantánea del modelo de contexto para migraciones.
      * `Entity.cs`: Clase `Entity` que hereda de `AggregateRoot` con propiedad `Name`.
      * `EntityId.cs`: Clase `EntityId` que hereda de `NewType` con método para generar identificadores aleatorios.
      * `TEntity.cs`: Clase `TEntity` con propiedades `Id` y `Name`.

--- tests-app/VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests/EfCoreRepositoryTests.cs
+++ tests-app/VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests/EfCoreRepositoryTests.cs
@@ -1,59 +1,249 @@
-using System.Linq.Expressions;
-using System.Runtime.Serialization;
+using FluentAssertions;
 using LanguageExt;
+using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
-using VSlices.Domain;
+using VSlices.Base;
+using VSlices.Base.Traits;
+using VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests.DataAccess;
+using VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests.Models;
 
 namespace VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests;
 
-public class EfCoreRepositoryTests
+public class EfCoreRepositoryTests(Fixture fixture) : IClassFixture<Fixture>
 {
-    public sealed class EntityId : NewType<EntityId, int>
+    private readonly Fixture _fixture = fixture;
+
+    [Fact]
+    public async Task Get_Success_ShouldReturnExistingEntityAsync()
     {
-        public EntityId(int value) : base(value) { }
+        // Arrange
+        EntityRepository repository = new();
 
-        public EntityId(SerializationInfo info, StreamingContext context) : base(info, context) { }
-    }
-
-    public sealed class Entity(EntityId id) : AggregateRoot<EntityId>(id)
-    {
-
-    }
-
-    public sealed class TEntity
-    {
-        public int Id { get; set; }
-    }
-
-    public sealed class Context : DbContext
-    {
-        public DbSet<TEntity> Entities => Set<TEntity>();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        Entity expEntity = new(EntityId.Random(), null);
+        TEntity entity = new()
         {
-            optionsBuilder.UseInMemoryDatabase("TestDb");
-        }
+            Id = expEntity.Id.Value
+        };
+
+        _fixture.Context.Add(entity);
+        await _fixture.Context.SaveChangesAsync();
+        _fixture.Context.ChangeTracker.Clear();
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        Eff<VSlicesRuntime, Entity> eff = repository.Get(EntityId.New(entity.Id));
+
+        // Act
+        Fin<Entity> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                             EnvIO.New());
+
+        // Assert
+        _ = result.BiMap(Succ: e =>
+                         {
+                             e.Id.Value.Should().Be(expEntity.Id.Value);
+                             e.Name.Should().BeNull();
+
+                             return e;
+                         },
+                         Fail: _ => throw new UnreachableException());
     }
 
-    public sealed class EntityRepository : EfCoreRepository<Context, Entity, EntityId, TEntity>
+    [Fact]
+    public void Get_Failure_ShouldThrow()
     {
-        protected override Expression<Func<TEntity, bool>> DomainKeySelector(EntityId id) => 
-            entity => entity.Id == id.Value;
+        // Arrange
+        EntityRepository repository = new();
 
-        protected override Entity ToDomain(TEntity projection) => new(EntityId.New(projection.Id));
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
 
-        protected override TEntity ToProjection(Entity root) =>
-            new()
-            {
-                Id = root.Id.Value
-            };
+        Eff<VSlicesRuntime, Entity> eff = repository.Get(EntityId.Random());
+
+        // Act
+        Fin<Entity> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                             EnvIO.New());
+
+        // Assert
+        _ = result.BiMap<int>(Succ: e => throw new UnreachableException(),
+                         Fail: e =>
+                         {
+                             e.IsExceptional.Should()
+                              .BeTrue();
+
+                             e.ToException()
+                              .Should()
+                              .BeOfType<InvalidOperationException>()
+                              .Subject.Message.Should()
+                              .Be("Sequence contains no elements.");
+
+                             return e;
+                         });
 
     }
 
     [Fact]
-    public void Get_Success_ShouldReturnExistingProperty()
+    public async Task GetOrOption_Success_ShouldReturnExistingEntityAsync()
     {
+        // Arrange
+        EntityRepository repository = new();
+
+        Entity expEntity = new(EntityId.Random(), null);
+        TEntity entity = new()
+        {
+            Id = expEntity.Id.Value
+        };
+
+        _fixture.Context.Add(entity);
+        await _fixture.Context.SaveChangesAsync();
+        _fixture.Context.ChangeTracker.Clear();
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        Eff<VSlicesRuntime, Option<Entity>> eff = repository.GetOrOption(EntityId.New(entity.Id));
+
+        // Act
+        Fin<Option<Entity>> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                                             EnvIO.New());
+
+        // Assert
+        _ = result.BiMap(Succ: e =>
+                         {
+                             e.IsSome.Should().BeTrue();
+                             e.IsNone.Should().BeFalse();
+                             e.IfSome(entity =>
+                             {
+                                 entity.Id.Value.Should().Be(expEntity.Id.Value);
+                                 entity.Name.Should().BeNull();
+                             });
+
+                             return e;
+                         },
+                         Fail: _ => throw new UnreachableException());
+    }
+
+    [Fact]
+    public void GetOrOption_Success_ShouldReturnNone()
+    {
+        // Arrange
+        EntityRepository repository = new();
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        var eff = repository.GetOrOption(EntityId.Random());
+
+        // Act
+        var result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                             EnvIO.New());
+
+        // Assert
+        _ = result.BiMap(Succ: e =>
+                         {
+                             e.IsSome.Should().BeFalse();
+                             e.IsNone.Should().BeTrue();
+
+                             return e;
+                         },
+                         Fail: _ => throw new UnreachableException());
 
     }
+
+    [Fact]
+    public async Task AddAndSaveChanges_Success_ShouldThrow()
+    {
+        // Arrange
+        EntityRepository repository = new();
+        TestUnitOfWork   unitOfWork = new(repository);
+        Entity           entity     = new Entity(EntityId.Random(), null);
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        Eff<VSlicesRuntime, Unit> eff = from _ in unitOfWork.Entities.Add(entity)
+                                        from _1 in unitOfWork.SaveChanges()
+                                        select Prelude.unit;
+
+        // Act
+        Fin<Unit> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                                   EnvIO.New());
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+
+        var context = _fixture.Context;
+        var created = await context.Entities
+                                   .Where(e => e.Id == entity.Id.Value)
+                                   .SingleAsync();
+
+        created.Id.Should().Be(entity.Id.Value);
+        created.Name.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task UpdateAndSaveChanges_Success_ShouldThrow()
+    {
+        // Arrange
+        var              context       = _fixture.Context;
+
+        EntityId         id            = EntityId.Random();
+        EntityRepository repository    = new();
+        TestUnitOfWork   unitOfWork    = new(repository);
+        Entity           updatedEntity = new(id, "Ahora si existe");
+
+        context.Entities.Add(new TEntity { Id = id.Value, Name = null });
+        await context.SaveChangesAsync();
+        context.ChangeTracker.Clear();
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        Eff<VSlicesRuntime, Unit> eff = from _ in unitOfWork.Entities.Update(updatedEntity)
+                                        from _1 in unitOfWork.SaveChanges()
+                                        select Prelude.unit;
+
+        // Act
+        Fin<Unit> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                                   EnvIO.New());
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+
+        var updated = await context.Entities
+                                   .Where(e => e.Id == updatedEntity.Id.Value)
+                                   .SingleAsync();
+
+        updated.Id.Should().Be(id.Value);
+        updated.Name.Should().Be(updatedEntity.Name);
+    }
+
+    [Fact]
+    public async Task DeleteAndSaveChanges_Success_ShouldThrow()
+    {
+        // Arrange
+        var context = _fixture.Context;
+
+        EntityId         id            = EntityId.Random();
+        EntityRepository repository    = new();
+        TestUnitOfWork   unitOfWork    = new(repository);
+        Entity           updatedEntity = new(id, null);
+
+        context.Entities.Add(new TEntity { Id = id.Value, Name = null });
+        await context.SaveChangesAsync();
+        context.ChangeTracker.Clear();
+
+        DependencyProvider dependencyProvider = new(_fixture.Provider);
+
+        Eff<VSlicesRuntime, Unit> eff = from _ in unitOfWork.Entities.Delete(updatedEntity)
+                                        from _1 in unitOfWork.SaveChanges()
+                                        select Prelude.unit;
+
+        // Act
+        Fin<Unit> result = eff.Run(VSlicesRuntime.New(dependencyProvider),
+                                   EnvIO.New());
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+
+        var exist = await context.Entities
+                                   .Where(e => e.Id == updatedEntity.Id.Value)
+                                   .AnyAsync();
+
+        exist.Should().BeFalse();
+    }
 }
- 
---
+++ tests-app/VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests/Fixture.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+using VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests.DataAccess;
+
+namespace VSlices.Infrastructure.Domain.EntityFrameworkCore.IntegTests;
+
+public sealed class Fixture : IAsyncLifetime
+{
+    private MsSqlContainer _container;
+    private Context _context;
+    private IServiceProvider _provider;
+
+    public Context Context => _context;
+
+    public IServiceProvider Provider => _provider;
+
+    public async Task InitializeAsync()
+    {
+        _container = new MsSqlBuilder()
+                     .WithPassword("H4lv.2024IIXD")
+                     .Build();
+
+        await _container.StartAsync();
+
+        DbContextOptions<Context> opts = new DbContextOptionsBuilder<Context>()
+                                         .UseSqlServer(_container.GetConnectionString())
+                                         .Options;
+
+        _context = new Context(opts);
+
+        await _context.Database.MigrateAsync();
+
+        _provider = new ServiceCollection()
+                    .AddDbContext<Context>(options => options.UseSqlServer(_container.GetConnectionString()))
+                    .BuildServiceProvider();
+
+
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+        await _context.DisposeAsync();
+    }
+}
