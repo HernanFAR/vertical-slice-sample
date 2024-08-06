87568ddb99258636f3ac41bc3d10459f1e58959f
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-08-06 17:52:16 -0400

    Remove VSlices.CrossCutting.Pipeline and VSlices.Core
    Removed the VSlices.CrossCutting.Pipeline project and its related build configurations from the solution file (VSlices.sln). Deleted the VSlices.Core.csproj file. Modified AssemblyInfo.cs to remove global using directives for LanguageExt. Updated AssemblyConfig.cs to remove InternalsVisibleTo attributes for unit test projects. Noted that the VSlices.Core.UseCases.UnitT project entry in the solution file appears truncated or corrupted.

--- src-app/VSlices.CrossCutting.Pipeline/Properties/AssemblyConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("VSlices.CrossCutting.Pipeline.UnitTests")]
-[assembly: InternalsVisibleTo("VSlices.CrossCutting.Pipeline.ExceptionHandling.UnitTests")]
-[assembly: InternalsVisibleTo("VSlices.CrossCutting.Pipeline.FluentValidation.UnitTests")]
-
